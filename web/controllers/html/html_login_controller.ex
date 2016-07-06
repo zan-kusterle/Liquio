b63bb12fb4e5f9ecd12bdda9bf18c67a506e8e09
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-06 23:58:10 +0200

    flash messages css

--- web/controllers/html/html_login_controller.ex
+++ web/controllers/html/html_login_controller.ex
@@ -12,7 +12,7 @@ defmodule Democracy.HtmlLoginController do
 		identity = Repo.get_by(Identity, username: username)
 		if identity != nil and identity.token == password do
 			conn
-			|> put_flash(:info, "Logged in.")
+			|> put_flash(:info, "Hello, #{identity.name}")
 			|> Guardian.Plug.sign_in(identity)
 			|> redirect(to: html_identity_path(conn, :show, identity.id))
 		else
@@ -23,7 +23,7 @@ defmodule Democracy.HtmlLoginController do
 
 	def delete(conn, _params) do
 		Guardian.Plug.sign_out(conn)
-		|> put_flash(:info, "Logged out successfully.")
+		|> put_flash(:info, "Goodbye")
 		|> redirect(to: html_login_path(conn, :create))
 	end
 end
