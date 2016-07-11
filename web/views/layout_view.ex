f91dbfc274d5b0eed60178c8040f2b68205d17cb
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-12 01:28:39 +0200

    not in trust metric indicator

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule Democracy.LayoutView do
-  use Democracy.Web, :view
+	use Democracy.Web, :view
+
+	alias Democracy.TrustMetric
+
+	def is_in_trust_metric?(user) do
+		case TrustMetric.get(user.trust_metric_url || Democracy.TrustMetric.default_trust_metric_url()) do
+			{:ok, trust_identity_ids} ->
+				MapSet.member?(trust_identity_ids, to_string(user.id))
+			{:error, message} ->
+				false
+		end
+	end
 end
