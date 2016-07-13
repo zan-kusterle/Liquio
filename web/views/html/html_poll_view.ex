99ef0ebc9e9c39805881e86d2fb73f6d9ae5eaa6
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-13 14:33:04 +0200

    enocelomestni zapis :D

--- web/views/html/html_poll_view.ex
+++ web/views/html/html_poll_view.ex
@@ -25,7 +25,16 @@ defmodule Democracy.HtmlPollView do
 	end
 
 	def number_format(x) do
-		:erlang.float_to_binary(x, [:compact, { :decimals, 2 }])
-		|> String.replace(".0", "")
+		{value, suffix} = cond do
+			x > 999999999.999999 ->
+				{x / 1000000000, " x 10<sup>9</sup>"}
+			x > 999999.999999 ->
+				{x / 1000000, " x 10<sup>6</sup>"}
+			true ->
+				{x, ""}
+		end
+
+		(:erlang.float_to_binary(value, [:compact, { :decimals, 2 }])
+		|> String.replace(".0", ""))  <> suffix
 	end
 end
