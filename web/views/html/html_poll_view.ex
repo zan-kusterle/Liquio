546075b2473688e4b9170753fb342f0f908da540
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-13 13:58:14 +0200

    quantity polls no score color

--- web/views/html/html_poll_view.ex
+++ web/views/html/html_poll_view.ex
@@ -1,11 +1,16 @@
 defmodule Democracy.HtmlPollView do
 	use Democracy.Web, :view
 
-	def score_color(score) do
-		cond do
-			score < 0.5 -> "rgb(255, 164, 164)"
-			score < 0.75 -> "rgb(249, 226, 110)"
-			true -> "rgb(140, 232, 140)"
+	def score_color(poll) do
+		if poll.choice_type == "probability" do
+			score = poll.results.mean
+			cond do
+				score < 0.5 -> "rgb(255, 164, 164)"
+				score < 0.75 -> "rgb(249, 226, 110)"
+				true -> "rgb(140, 232, 140)"
+			end
+		else
+			"#ddd"
 		end
 	end
 
