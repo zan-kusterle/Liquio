4d3ed94464541c4e98b5c4b683c26ed31e0aa3ec
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-14 17:41:53 +0200

    minor

--- web/plugs/title.ex
+++ web/plugs/title.ex
@@ -6,6 +6,7 @@ defmodule Democracy.Plugs.Title do
 
 	def call(conn, {assign_atom, query_name}) do
 		if value = Map.get(conn.params, query_name) do
+			value = value |> String.trim
 			%{conn | params: conn.params |> Map.merge(conn.query_params) |> Map.merge(%{assign_atom => value})}
 		else
 			conn
