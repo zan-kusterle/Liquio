76d6cd93251d7b27fab37610df88d6e4ac77e1e3
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-19 21:47:47 +0200

    votes polish

--- web/views/utils/number_format.ex
+++ web/views/utils/number_format.ex
@@ -14,8 +14,8 @@ defmodule Democracy.NumberFormat do
 	def score_format(score, choice_type) do
 		if choice_type == "probability" do
 			case score do
-				0.0 -> raw('<span style="color: rgb(255, 164, 164);">NEGATIVE</span>')
-				1.0 -> raw('<span style="color: rgb(140, 232, 140);">POSITIVE</span>')
+				0.0 -> raw('<span style="color: rgb(255, 97, 97);">NEGATIVE</span>')
+				1.0 -> raw('<span style="color: rgb(111, 206, 111);">POSITIVE</span>')
 				x -> raw("#{format_number(x * 100)}%")
 			end
 		else
