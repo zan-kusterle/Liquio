348a8ffaf21f078f4d0b2bd895a559fb01602979
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-20 03:27:55 +0200

    minor

--- web/controllers/html/html_explore_controller.ex
+++ web/controllers/html/html_explore_controller.ex
@@ -4,18 +4,21 @@ defmodule Democracy.HtmlExploreController do
 	def index(conn, _) do
 		conn
 		|> render "index.html",
+			heading: "List of all polls",
 			polls: Poll.all |> Repo.all
 	end
 
 	def show(conn, %{"id" => topic}) do
 		conn
 		|> render "index.html",
+			heading: "POLLS WITH TOPIC #{topic}",
 			polls: Poll.by_topic(topic |> String.downcase) |> Repo.all
 	end
 
 	def search(conn, %{"query" => query}) do
 		conn
 		|> render "index.html",
+			heading: "Showing most relevant results",
 			query: query,
 			polls: Poll.search(Poll, query) |> Repo.all
 	end
