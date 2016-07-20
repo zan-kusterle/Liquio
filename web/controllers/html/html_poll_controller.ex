f73d4fdcbd67694a79e9db7f127645ba85f22fc5
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-20 21:12:43 +0200

    fix

--- web/controllers/html/html_poll_controller.ex
+++ web/controllers/html/html_poll_controller.ex
@@ -34,7 +34,7 @@ defmodule Democracy.HtmlPollController do
 		:user => {Plugs.CurrentUser, [require: false]},
 		:poll => {Plugs.ItemParam, [schema: Poll, name: "id"]},
 	},
-	def show(conn, params = %{:user => user, :poll => poll}) do
+	def show(conn, %{:user => user, :poll => poll}) do
 		calculation_opts = get_calculation_opts_from_conn(conn)
 		conn
 		|> put_resp_header("Cache-Control", "no-cache, no-store, must-revalidate, max-age=0")
