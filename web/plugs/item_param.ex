7f0fdb5cfb56f54189eaa2adc7cf46834c4afdc5
Zan Kusterle <zan.kusterle@gmail.com>
2016-07-26 05:08:39 +0200

    simpler code

--- web/plugs/item_param.ex
+++ web/plugs/item_param.ex
@@ -1,17 +1,8 @@
 defmodule Democracy.Plugs.ItemParam do
 	def handle(conn, value, opts) do
 		item = Democracy.Repo.get(opts[:schema], value)
-		if item != nil do
-			if validator = opts[:validator] do
-				if validator.(item) do
-					{:ok, item}
-				else
-					{:error, :not_found, opts[:message] || "Not found"}
-				end
-			else
-				{:ok, item}
-			end
-
+		if item != nil and (opts[:validator] == nil or opts[:validator].(item)) do
+			{:ok, item}
 		else
 			{:error, :not_found, opts[:message] || "Not found"}
 		end
