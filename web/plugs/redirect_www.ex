7a99f228dab6c7cd64df040b1f95b9f7d9331237
Zan Kusterle <zan.kusterle@gmail.com>
2016-08-08 18:57:27 +0200

    fix

--- web/plugs/redirect_www.ex
+++ web/plugs/redirect_www.ex
@@ -6,7 +6,7 @@ defmodule Liquio.Plugs.RedirectWww do
 	def call(conn, opts) do
 		if conn.host |> String.starts_with?("www.") do
 			port = if conn.port != 80 and conn.port != 443 do ":#{conn.port}" else "" end
-			query = if Map.has_key?(conn, :query_string) do "?#{conn.query_string}" else "" end
+			query = if String.length(Map.get(conn, :query_string, "")) > 0 do "?#{conn.query_string}" else "" end
 			url = to_string(conn.scheme) <> "://" <> String.trim_leading(conn.host, "www.") <> port <> conn.request_path <> query
 			conn
 			|> Phoenix.Controller.redirect(external: url)
