a61187172fe071e4c6c644efa3c5dd45fc639ed0
Zan Kusterle <zan.kusterle@gmail.com>
2016-08-26 01:59:38 +0200

    removed is in trust metric layout indicator

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,9 +1,4 @@
 defmodule Liquio.LayoutView do
 	use Liquio.Web, :view
 	
-	def is_in_trust_metric?(conn) do
-		user = Guardian.Plug.current_resource(conn)
-		{_url, trust_identity_ids} = Liquio.Controllers.Helpers.get_trust_identity_ids(conn)
-		MapSet.member?(trust_identity_ids, to_string(user.id))
-	end
 end
