17c41c4aea72edfacfb14bcfe5a401a14faa5953
Zan Kusterle <zan.kusterle@gmail.com>
2017-01-07 02:15:27 +0100

    elixir 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Liquio.Mixfile do
 		[
 			app: :liquio,
 			version: "0.0.1",
-			elixir: "~> 1.3",
+			elixir: "~> 1.4",
 			elixirc_paths: elixirc_paths(Mix.env),
 			compilers: [:phoenix, :gettext] ++ Mix.compilers,
 			build_embedded: Mix.env == :prod,
