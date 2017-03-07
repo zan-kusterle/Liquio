cc1fc89b9e3af9f5d5cbbb78f81d51d13abd8aed
Zan Kusterle <zan.kusterle@gmail.com>
2017-03-07 17:09:54 +0100

    distillery config

--- rel/config.exs
+++ rel/config.exs
@@ -1,36 +1,18 @@
 use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
-    default_release: :default,
-    # This sets the default environment used by `mix release`
-    default_environment: :dev
-
-# For a full list of config options for both releases
-# and environments, visit https://hexdocs.pm/distillery/configuration.html
-
-
-# You may define one or more environments in this file,
-# an environment's settings will override those of a release
-# when building in that environment, this combination of release
-# and environment configuration is called a profile
+	default_release: :liquio,
+	default_environment: Mix.env,
 
 environment :dev do
-  set dev_mode: true
-  set include_erts: false
-  set cookie: :"._vZneBV[:10GwokqhODQLImTU3jn9L4t%0=GgC3@>4K^poFuJsd>KAh`ID@7mf-"
+	set dev_mode: true set include_erts: false
+	set include_system_libs: false
+	set cookie: :dev
 end
-
 environment :prod do
-  set include_erts: true
-  set include_src: false
-  set cookie: :"._vZneBV[:10GwokqhODQLImTU3jn9L4t%0=GgC3@>4K^poFuJsd>KAh`ID@7mf-"
+	set include_erts: true
+	set include_system_libs: true
+	set cookie: :prod
 end
-
-# You may define one or more releases in this file.
-# If you have not set a default release, or selected one
-# when running `mix release`, the first release in the file
-# will be used by default
 
 release :liquio do
-  set version: current_version(:liquio)
+	set version: current_version(:liquio)
 end
-
