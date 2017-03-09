d40ee184ac852dae668d5fa5159e1adf9c682b74
Zan Kusterle <zan.kusterle@gmail.com>
2017-03-09 02:59:34 +0100

    force dev env

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
-				NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+				NODE_ENV: 'development'
 			}
 		}),
 		new ExtractTextPlugin("css/app.css"),
