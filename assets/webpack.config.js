c34dea2d33d9c2621805e97e7a21af9269cd0f74
Zan Kusterle <zan.kusterle@gmail.com>
2017-03-09 03:00:24 +0100

    a

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env': {
-				NODE_ENV: 'development'
+				NODE_ENV: JSON.stringify('development')
 			}
 		}),
 		new ExtractTextPlugin("css/app.css"),
