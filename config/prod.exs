6b27efbd2489f4310d4416b570b60eddaa60f72d
Zan Kusterle <zan.kusterle@gmail.com>
2017-06-11 18:34:20 +0200

    enable server

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,8 @@ use Mix.Config
 config :liquio, Liquio.Web.Endpoint,
 	http: [port: 4000],
 	url: [host: "liqu.io", port: 443],
-	cache_static_manifest: "priv/static/cache_manifest.json"
+	cache_static_manifest: "priv/static/cache_manifest.json",
+	server: true
 
 config :logger, level: :info
 
